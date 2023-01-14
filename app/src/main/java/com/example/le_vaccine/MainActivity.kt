970ed7c56a5b3302406500296f13d88386af2e5b
Otherwa <67428572+Atharv2002@users.noreply.github.com>
2023-01-14 13:29:34 +0530

    Location Permission Done

--- app/src/main/java/com/example/le_vaccine/MainActivity.kt
+++ app/src/main/java/com/example/le_vaccine/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.le_vaccine
 
+import android.content.pm.PackageManager
 import android.os.Build
 import android.os.Bundle
 import android.webkit.GeolocationPermissions
@@ -7,7 +8,10 @@ import android.webkit.WebChromeClient
 import android.webkit.WebViewClient
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.jar.Manifest
 
 
 class MainActivity : AppCompatActivity() {
@@ -19,7 +23,38 @@ class MainActivity : AppCompatActivity() {
         supportActionBar?.hide()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-       //webview
+
+        val LOCATION_PERMISSION_REQUEST_CODE = 1
+
+        if (ContextCompat.checkSelfPermission(this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION)
+            != PackageManager.PERMISSION_GRANTED) {
+
+            // Permission is not granted
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    android.Manifest.permission.ACCESS_FINE_LOCATION)) {
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+            } else {
+                // No explanation needed, we can request the permission.
+                ActivityCompat.requestPermissions(this,
+                    arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION),
+                    LOCATION_PERMISSION_REQUEST_CODE)
+
+                // LOCATION_PERMISSION_REQUEST_CODE is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            }
+        } else {
+            // Permission has already been granted
+        }
+
+
+
+
+        //webview
         webviewsetup()
     }
 
